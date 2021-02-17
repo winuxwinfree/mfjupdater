59986b5b73caf9e53923b799129094d919806e44
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-17 17:00:05 +0100

    añadida opción de actualizar el sistema
    + se describe mejor que hace el script

--- updater.sh
+++ updater.sh
@@ -2,8 +2,10 @@
 clear
 echo "Feature under development. "
 echo "We test all updates to make sure they don't break anything, use this tool instead of pacman -Syyu... if you don't want to take risks."
-echo "This script will switch the repositories to the stable branch in 10 seconds. Close this window if you prefer to continue using the unstable one."
-echo "Commands to be executed: sudo pacman-mirrors -aS stable. Important to avoid bluetooth problems and high cpu consumption on a core. Problem: Manjaro ARM update failed, “brcm-patchram-plus” and “pi-bluetooth” conflicting https://forum.manjaro.org/t/manjaro-arm-update-failed-brcm-patchram-plus-and-pi-bluetooth-conflicting/42902"
-sleep 10
+echo "Commands to be executed:"
+echo "1.sudo pacman-mirrors -aS stable. Important to avoid bluetooth problems and high cpu consumption on a core. Problem: Manjaro ARM update failed, “brcm-patchram-plus” and “pi-bluetooth” conflicting-https://forum.manjaro.org/t/manjaro-arm-update-failed-brcm-patchram-plus-and-pi-bluetooth-conflicting/42902"
+echo "2.sudo pacman-Syyu"
+echo "You have 15 seconds to close this window if you do not want to switch to the stable branch and upgrade the system."
+sleep 15
 sudo pacman-mirrors -aS stable
 sleep 5
