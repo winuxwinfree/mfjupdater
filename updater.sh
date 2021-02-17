ea632bae3612ffdbc71df4b036fb3222b0b0239c
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-17 16:30:30 +0100

    sudo pacman-mirrors -aS (branch) switcher

--- updater.sh
+++ updater.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 clear
 echo "Feature under development."
+echo "This script will switch the repositories to the stable branch in 10 seconds. Close this window if you prefer to continue using the unstable one."
+echo "Command to be executed: sudo pacman-mirrors -aS stable."
+sleep 10
+sudo pacman-mirrors -aS stable
 sleep 5
