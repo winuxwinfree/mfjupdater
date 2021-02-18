81ef19591ba6bcf752661642f60c8d17fb28d596
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-18 21:50:35 +0100

    updated pacman mirrors command error msg

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ echo "We test all updates to make sure they don't break anything, use this tool 
 echo "Commands to be executed:"
 echo "1.sudo pacman-mirrors -aS stable. Important to avoid bluetooth problems and high cpu consumption on a core. Problem: Manjaro ARM update failed, “brcm-patchram-plus” and “pi-bluetooth” conflicting-https://forum.manjaro.org/t/manjaro-arm-update-failed-brcm-patchram-plus-and-pi-bluetooth-conflicting/42902"
 echo "2.sudo pacman-Syyu"
-sudo pacman-mirrors -aS stable || echo "You are already using the stable branch."
+sudo pacman-mirrors -aS stable || echo "You are already using the stable branch Or the command wasn't executed correctly."
 sudo pacman -Syyu  || echo "sudo pacman -Syyu - Error"
 echo "Done"
 sleep 15
