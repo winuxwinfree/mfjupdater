122900863d1a606028246a6711c6085ee30f1e86
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-20 16:24:47 +0100

    \n

--- updater.sh
+++ updater.sh
@@ -7,5 +7,5 @@ echo "1.sudo pacman-mirrors -aS stable."
 echo "2.sudo pacman-Syyu"
 sudo pacman-mirrors -aS stable || echo "You are already using the stable branch or the command wasn't executed correctly."
 sudo pacman -Syyu  || echo "sudo pacman -Syyu - Error"
-echo "Done, you can close this window."
+echo "\nDone, you can close this window."
 sleep 99999
