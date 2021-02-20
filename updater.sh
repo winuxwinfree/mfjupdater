c8c39295dfac1cab5ce8b099878dafeeb52c69de
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-20 17:22:19 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,9 +2,6 @@
 clear
 cd
 rm -f updater*
-echo "Feature under development."
-echo "1.sudo pacman-mirrors -aS stable."
-echo "2.sudo pacman-Syyu"
 sudo pacman-mirrors -aS stable || echo "You are already using the stable branch or the command wasn't executed correctly."
 sudo pacman -Syyu  || echo "sudo pacman -Syyu - Error"
 echo "\nDone, you can close this window."
