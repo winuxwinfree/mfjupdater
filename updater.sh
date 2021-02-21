cc8c34ca09305f8af2ebf01443fb2ddff9b89873
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-02-21 07:02:00 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -34,7 +34,8 @@ case $opcion in
 1) sudo pacman-mirrors -aS stable || echo "You are already using the stable branch or the command can't be executed.";
    sudo pacman -Syyu  || echo "sudo pacman -Syyu - Error";;
 2) sudo pacman -Syyu  || echo "sudo pacman -Syyu - Error";;
-3) sudo pacman -Scc && paccache -r && sudo pacman -Rns $(pacman -Qtdq);;
+3) paccache -r & echo "Attention, read the warnings before
+" && sudo pacman -Rns $(pacman -Qtdq) & sudo pacman -Scc;;
 
 a) install-tor;;
 q) echo "Done, closing.";
